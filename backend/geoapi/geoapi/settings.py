722d7dad9194615453b645f62b4555f7dfeae7f9
radha krishna <kssvrk@gmail.com>
2022-06-09 07:02:42 +0530

    added partitions

--- backend/geoapi/geoapi/settings.py
+++ backend/geoapi/geoapi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis', # GeoDjango
     'countries',
+    'bhoosearch',
     'rest_framework'
 ]
 
--- backend/server.sh
+++ backend/server.sh
@@ -1,3 +1,4 @@
 cd /code/geoapi
-python manage.py makemigrations && python manage.py migrate;
-python manage.py runserver 0.0.0.0:8000
+gunicorn --timeout=1000 --bind=0.0.0.0:8000 geoapi.wsgi
+#python manage.py makemigrations && python manage.py migrate;
+#python manage.py runserver 0.0.0.0:8001
